8094c9f892536f686883e118244d395ed0955fcf
Adam Stankiewicz <agstanki@gmail.com>
2020-10-11 16:05:40 -0400

    rename function

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -3,7 +3,7 @@ import { Container } from '@edx/paragon';
 
 import NotFoundBase from '../../components/not-found';
 
-export default function Analytics() {
+export default function NotFound() {
   return (
     <Container className="py-3" fluid>
       <NotFoundBase />
